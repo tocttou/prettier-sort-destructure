d7f661337df87553ea28e448722b000bb055da70
Ashish Chaudhary <ashish.chaudhary@rapyuta-robotics.com>
2019-08-08 16:12:45 +0530

    fixed nested keys case

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 var walk = require("estree-walker").walk;
 
+function sortKeys(node) {
+  var properties = node.properties;
+  node.properties = properties.sort(function(a, b) {
+    return a.key.name.localeCompare(b.key.name);
+  });
+  node.properties.map(function(prop) {
+    delete prop.start;
+    delete prop.end;
+  });
+}
+
 module.exports = function(text, parser) {
   var ast = parser.babel(text);
   walk(ast, {
@@ -8,14 +19,13 @@ module.exports = function(text, parser) {
         node.type === "VariableDeclarator" &&
         node.id.type === "ObjectPattern"
       ) {
-        var properties = node.id.properties;
-        node.id.properties = properties.sort(function(a, b) {
-          return a.key.name.localeCompare(b.key.name);
-        });
-        node.id.properties.map(function(prop) {
-          delete prop.start;
-          delete prop.end;
-        });
+        sortKeys(node.id);
+      } else if (
+        node.type === "ObjectProperty" &&
+        node.key.type === "Identifier" &&
+        node.value.type === "ObjectPattern"
+      ) {
+        sortKeys(node.value);
       }
     }
   });
